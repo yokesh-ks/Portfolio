281e198f3f5f3d0ee50b172f2580461c4fcdcd3c
yokesh_ks <yokesh.ks@increscotech.com>
2023-04-28 21:53:40 +0530

    feat(tailwind): tailwind css install

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#1b1b1b",
+        light: "#f5f5f5",
+        primary: "#B63E96", // 240,86,199
+        primaryDark: "#58E6D9", // 80,230,217
+      }
+    },
+  },
+  plugins: [],
+}
+
