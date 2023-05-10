65344c6e8bcd52eeee607c768f2165aa9bc99bde
yokesh_ks <yokesh.ks@increscotech.com>
2023-05-10 19:21:57 +0530

    feat(hero section): added Resume and contact button

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import SvgGithub from "./Github"
 import SvgLinkedin from './Linkedin'
+import SvgLinkArrow from './LinkArrow'
 
 
 export const Icon = (props) => {
@@ -9,5 +10,8 @@ export const Icon = (props) => {
     }
     if (props.name === 'Linkedin') {
         return <SvgLinkedin {...props} />
+    }
+    if (props.name === 'LinkArrow') {
+        return <SvgLinkArrow {...props} />
     }
 }
