21facc681ffbf9ef2a6e4abf635d392cf0d2ebf5
yokesh_ks <yokesh.ks@increscotech.com>
2023-05-10 20:30:14 +0530

    feat(home): hire Me button Added

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import SvgGithub from "./Github"
 import SvgLinkedin from './Linkedin'
 import SvgLinkArrow from './LinkArrow'
+import SVGCircularText from './CircularText'
 
 
 export const Icon = (props) => {
@@ -13,5 +14,8 @@ export const Icon = (props) => {
     }
     if (props.name === 'LinkArrow') {
         return <SvgLinkArrow {...props} />
+    }
+    if (props.name === 'CircularText') {
+        return <SVGCircularText {...props} />
     }
 }
